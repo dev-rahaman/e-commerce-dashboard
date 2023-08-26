7a08be6576ae5acc5484e6109f5fdced03d2ccbc
dev-rahaman <abdurrahamansultany.main@gmail.com>
2023-08-27 02:06:01 +0600

    change domain

--- src/components/Recaptcha.jsx
+++ src/components/Recaptcha.jsx
@@ -6,9 +6,15 @@ const Recaptcha = () => {
     console.log("Captcha value:", value);
   }
 
+  // site key  6LevEtknAAAAACuGKzOliDa_hRQ9RF9c-Os-B8gb
+  // secret key   6LevEtknAAAAAD6QwMamqQRapkF9Tf-vsNR8gf9L
+
+  // site key  6Lc9GNknAAAAAHPMm4DNNdq8u7e23UXvW4jqhtIP
+  // secret key   6Lc9GNknAAAAALU_LiSYm-hsUBLF-Jgdiias4nEp
+
   return (
     <ReCAPTCHA
-      sitekey="6LevEtknAAAAACuGKzOliDa_hRQ9RF9c-Os-B8gb"
+      sitekey="6Lc9GNknAAAAAHPMm4DNNdq8u7e23UXvW4jqhtIP"
       onChange={onChange}
     />
   );
